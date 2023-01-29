c5e8f841d874f8449957b784821c770624bf4cca
rotsee <leo_wallentin@hotmail.com>
2023-01-29 13:57:05 +0100

    fix Åsas computer bug

--- app.vue
+++ app.vue
@@ -1,22 +1,24 @@
 <template>
   <v-app>
-    <v-container>
-      <NuxtPage />
-    </v-container>
-  <v-bottom-navigation>
-    <v-btn variant="text">
-      Om oss
-    </v-btn>
-    <v-btn variant="text">
-      Discord
-    </v-btn>
-    <v-btn variant="text">
-      Reddit
-    </v-btn>
-    <v-btn variant="text">
-      Logga
-    </v-btn>
-  </v-bottom-navigation>
+    <v-main>
+      <v-container>
+        <NuxtPage />
+      </v-container>
+    </v-main>
+    <v-bottom-navigation>
+      <v-btn variant="text">
+        Om oss
+      </v-btn>
+      <v-btn variant="text">
+        Discord
+      </v-btn>
+      <v-btn variant="text">
+        Reddit
+      </v-btn>
+      <v-btn variant="text">
+        Logga
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
