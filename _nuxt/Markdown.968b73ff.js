6d8653402ce5c0de796da6b57e3b7e171532f3e0
CI <CI@example.com>
2023-03-14 19:37:11 +0000

    deploy

---
+++ _nuxt/Markdown.968b73ff.js
@@ -0,0 +1,1 @@
+import s from"./ContentSlot.bc8295e8.js";import{a as o,b as f,f as u,j as m}from"./entry.6ec49ec1.js";import"./utils.ab20eeef.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=m(),{between:n,default:a}=f(),r=u(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
